6d3abe1a61245180c58957ec5f99948e97938d2e
Dilfer123 <dilfernisha@gmail.com>
2022-12-20 18:58:22 +0530

    new code

--- src/main/java/org/git/HelloWorld.java
+++ src/main/java/org/git/HelloWorld.java
@@ -3,7 +3,9 @@ package org.git;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
+		//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
 		System.out.println("HelloWorld");
+		
+		System.out.println("hi");
 	}
 }
